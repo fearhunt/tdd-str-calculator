6e4e41e4688097f174fed21ec29db4b94da10a28
fearhunt <firhanmahdi@gmail.com>
2025-03-26 19:16:01 +0700

    feat: solve return sums with newline

--- strCalculator.ts
+++ strCalculator.ts
@@ -1,5 +1,5 @@
 export function add(strNum: string): number {
   if (strNum === '') return 0
-  const nums = strNum.split(',').map(Number)
+  const nums = strNum.split(/,|\n/).map(Number)
   return nums.reduce((sum, num) => sum + num, 0)
 }
